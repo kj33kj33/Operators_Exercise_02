4c1ff5fd5821bfc42615ffa94cc28b961f320c71
johndward01 <johndward01@gmail.com>
2023-04-05 21:31:08 -0500

    Add project files.

---
+++ Operators_Exercise_02/Program.cs
@@ -0,0 +1,33 @@
+﻿
+// TODO: use the compound add-assignment operator to add the 2 variables below 
+// Print the result to the console
+
+int add1 = 10;
+int add2 = 11;
+
+
+// TODO: use the compound subtract-assignment operator to subtract the 2 variables below 
+// Print the result to the console
+
+int sub1 = 10;
+int sub2 = 11;
+
+// TODO: use the compound multiply-assignment operator to multiply the 2 variables below 
+// Print the result to the console
+
+int mul1 = 10;
+int mul2 = 11;
+
+// TODO: use the compound divide-assignment operator to divide the 2 variables below 
+// Print the result to the console
+
+int div1 = 10;
+int div2 = 11;
+
+
+// TODO: use the compound modulo-assignment operator to modulo the 2 variables below 
+// Print the result to the console
+
+int mod1 = 10;
+int mod2 = 11;
+
