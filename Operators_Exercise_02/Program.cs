1df4782d10dd65e01b67c22d902bbdeed7ce0d18
johndward01 <johndward01@gmail.com>
2023-04-25 11:12:28 -0500

    update

--- Operators_Exercise_02/Program.cs
+++ Operators_Exercise_02/Program.cs
@@ -1,35 +1,35 @@
 ﻿
 // KEEPING IT 💯💯💯!
 
-// TODO: Use the assignment operator to initialize the variable below to 10
+// TODO: Use the assignment operator to initialize the variable below to 100
 // TODO: Print the result to the console
 int assign;
 
-// TODO: Declare and initialzie an integer variable called sum with a value of 0;
+// TODO: Declare and initialize an integer variable called sum with a value of 0;
 // TODO: Use the compound add-assignment operator to add the 2 variables 
 // TODO: Print the result to the console 
 int sum = 0;
 int add = 100;
 
-// TODO: Declare and initialzie an integer variable called difference with a value of 0;
+// TODO: Declare and initialize an integer variable called difference with a value of 0;
 // TODO: Use the compound subtract-assignment operator to subtract the 2 variables 
 // Print the result to the console
 int difference = 0;
 int sub = -100;
 
-// TODO: Declare and initialzie an integer variable called factor with a value of 10;
+// TODO: Declare and initialize an integer variable called factor with a value of 10;
 // TODO: Use the compound multiply-assignment operator to multiply the 2 variables  
 // Print the result to the console
 int factor = 10;
 int mul = 10;
 
-// TODO: Declare and initialzie an integer variable called quotient with a value of 1000;
+// TODO: Declare and initialize an integer variable called quotient with a value of 1000;
 // TODO: Use the compound divide-assignment operator to divide the 2 variables  
 // Print the result to the console
 int quotient = 1_000;
 int div = 10;
 
-// TODO: Declare and initialzie an integer variable called remainder with a value of 100;
+// TODO: Declare and initialize an integer variable called remainder with a value of 100;
 // TODO: Use the compound modulo-assignment operator to modulo the 2 variables below 
 // Print the result to the console
 int remainder = 100;
