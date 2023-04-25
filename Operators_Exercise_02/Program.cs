727bb3df5aba3354215b83b7ff628ef4ecf46c09
johndward01 <johndward01@gmail.com>
2023-04-25 11:07:07 -0500

    updated answer branch and TODO's

--- Operators_Exercise_02/Program.cs
+++ Operators_Exercise_02/Program.cs
@@ -1,37 +1,49 @@
 ﻿
-// TODO: use the assignment operator to initialize the variable below to 10
-int assign1;
+// KEEPING IT 💯💯💯
 
+// TODO: Use the assignment operator to initialize the variable below to 10
+// TODO: Print the result to the console
+int assign;
+assign = 100;
+Console.WriteLine(assign);
 
-// TODO: use the compound add-assignment operator to add the 2 variables below 
+// TODO: Declare and initialzie an integer variable called sum with a value of 0;
+// TODO: Use the compound add-assignment operator to add the 2 variables 
+// TODO: Print the result to the console 
+int sum = 0;
+int add = 100;
+sum += add;
+Console.WriteLine(sum);
+
+// TODO: Declare and initialzie an integer variable called difference with a value of 200;
+// TODO: Use the compound subtract-assignment operator to subtract the 2 variables 
 // Print the result to the console
+int difference = 0;
+int sub = -100;
+difference -= sub;
+Console.WriteLine(difference);
 
-int add1 = 10;
-int add2 = 11;
-
-
-// TODO: use the compound subtract-assignment operator to subtract the 2 variables below 
+// TODO: Declare and initialzie an integer variable called factor with a value of 10;
+// TODO: Use the compound multiply-assignment operator to multiply the 2 variables  
 // Print the result to the console
+int factor = 10;
+int mul = 10;
+factor *= mul;
+Console.WriteLine(factor);
 
-int sub1 = 10;
-int sub2 = 11;
-
-// TODO: use the compound multiply-assignment operator to multiply the 2 variables below 
+// TODO: Declare and initialzie an integer variable called quotient with a value of 1000;
+// TODO: Use the compound divide-assignment operator to divide the 2 variables  
 // Print the result to the console
+int quotient = 1_000;
+int div = 10;
+quotient /= div;
+Console.WriteLine(quotient);
 
-int mul1 = 10;
-int mul2 = 11;
-
-// TODO: use the compound divide-assignment operator to divide the 2 variables below 
+// TODO: Declare and initialzie an integer variable called remainder with a value of 0;
+// TODO: Use the compound modulo-assignment operator to modulo the 2 variables below 
 // Print the result to the console
-
-int div1 = 10;
-int div2 = 11;
-
-
-// TODO: use the compound modulo-assignment operator to modulo the 2 variables below 
-// Print the result to the console
-
-int mod1 = 10;
-int mod2 = 11;
+int remainder = 100;
+int mod = 500;
+remainder %= mod;
+Console.WriteLine(remainder);
 
