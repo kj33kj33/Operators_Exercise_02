2677950ddac1a9ccfe12d2f5b623d34a9727ef2a
James <jamesmcdaniel33@hotmail.com>
2023-05-15 09:57:51 -0500

    initial commit

--- Operators_Exercise_02/Program.cs
+++ Operators_Exercise_02/Program.cs
@@ -3,35 +3,46 @@
 
 // TODO: Use the assignment operator to initialize the variable below to 100
 // TODO: Print the result to the console
-int assign;
+int assign = 100;
+Console.WriteLine(assign);
 
 // TODO: Declare and initialize an integer variable called sum with a value of 0;
 // TODO: Use the compound add-assignment operator to add the 2 variables 
 // TODO: Print the result to the console 
 int sum = 0;
 int add = 100;
+sum += add;
+Console.WriteLine(sum);
 
 // TODO: Declare and initialize an integer variable called difference with a value of 0;
 // TODO: Use the compound subtract-assignment operator to subtract the 2 variables 
 // Print the result to the console
 int difference = 0;
 int sub = -100;
+difference -= sub;
+Console.WriteLine(difference);
 
 // TODO: Declare and initialize an integer variable called factor with a value of 10;
 // TODO: Use the compound multiply-assignment operator to multiply the 2 variables  
 // Print the result to the console
 int factor = 10;
 int mul = 10;
+factor *= mul;
+Console.WriteLine(factor);
 
 // TODO: Declare and initialize an integer variable called quotient with a value of 1000;
 // TODO: Use the compound divide-assignment operator to divide the 2 variables  
 // Print the result to the console
-int quotient = 1_000;
+int quotient = 1000;
 int div = 10;
+quotient /= div;
+Console.WriteLine(quotient);
 
 // TODO: Declare and initialize an integer variable called remainder with a value of 100;
 // TODO: Use the compound modulo-assignment operator to modulo the 2 variables below 
 // Print the result to the console
 int remainder = 100;
 int mod = 500;
+remainder %= mod;
+Console.WriteLine(remainder);
 
